02119ea6f645b0f8d46c17a556f3e6f5236f1fb5
Julian Kruithof <julian.kruithof@hva.nl>
2023-12-12 15:03:06 +0100

    finished part 2 of day 2

--- CubeConundrum/src/main/CubeConundrum.java
+++ CubeConundrum/src/main/CubeConundrum.java
@@ -1,7 +1,6 @@
 package main;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,10 +8,15 @@ import java.util.Scanner;
 public class CubeConundrum {
     public static void main(String[] args) {
         File file = new File("data/input.txt");
-        Map<String, Integer> possible = new HashMap<>() {{put("red", 12); put("blue", 14); put("green", 13);}};
+        Map<String, Integer> possible = new HashMap<>() {{
+            put("red", 12);
+            put("blue", 14);
+            put("green", 13);
+        }};
         try {
             Scanner scanner = new Scanner(file);
             int sumPartOne = 0;
+            int sumPartTwo = 0;
             int gameId = 0;
             while (scanner.hasNext()) {
                 //get all information
@@ -22,6 +26,11 @@ public class CubeConundrum {
                 gameId++;
 
                 boolean invalidGame = false;
+                Map<String, Integer> minValues = new HashMap<>() {{
+                    put("red", 0);
+                    put("green", 0);
+                    put("blue", 0);
+                }};
                 for (String hand : hands) {
                     String[] marbles = hand.split(",");
                     for (String marble : marbles) {
@@ -31,14 +40,18 @@ public class CubeConundrum {
                         if (possible.get(marbleColor) < amountOfMarbles) {
                             invalidGame = true;
                         }
+                        if (minValues.get(marbleColor) < amountOfMarbles) {
+                            minValues.put(marbleColor, amountOfMarbles);
+                        }
                     }
                 }
-
+                sumPartTwo += minValues.values().stream().reduce((integer, integer2) -> integer * integer2).orElse(0);
                 if (!invalidGame) {
                     sumPartOne += gameId;
                 }
             }
             System.out.println(sumPartOne);
+            System.out.println(sumPartTwo);
         } catch (Exception e) {
             e.printStackTrace();
         }
